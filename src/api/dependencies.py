f5fa7d0cfa95b2fc22ddf2b9e741d293c1cac854
NicMar <nicmarteam@gmail.com>
2026-08-16 18:29:56 +0300

    Update dependencies.py

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -8,6 +8,8 @@ from src.engines.mission.mission_engine import MissionEngine
 from src.agents.mission.mission_agent import MissionAgent
 from src.engines.followup.followup_engine import FollowUpEngine
 from src.agents.followup.followup_agent import FollowUpAgent
+from src.engines.partner.partner_engine import PartnerEngine
+from src.agents.partner.partner_agent import PartnerAgent
 
 
 def get_mission_agent() -> MissionAgent:
@@ -30,3 +32,9 @@ def get_followup_agent() -> FollowUpAgent:
 def get_followup_engine() -> FollowUpEngine:
     rule_engine = RuleEngine()
     return FollowUpEngine(rule_engine=rule_engine)
+
+
+def get_partner_agent() -> PartnerAgent:
+    rule_engine = RuleEngine()
+    partner_engine = PartnerEngine(rule_engine=rule_engine)
+    return PartnerAgent(partner_engine=partner_engine)
